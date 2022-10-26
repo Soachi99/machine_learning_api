6b67e726ccf7c50484e13e936d61005044e40801
Soachi <jupaim99@gmail.com>
2022-10-26 09:48:45 -0500

    Ajustes exigencias de iluminación

--- API/app.py
+++ API/app.py
@@ -14,120 +14,131 @@ app.config['UPLOAD_FOLDER'] = save_path
 imageList = []
 CORS(app, resources={r"*": {"origins": "*"}})
 
+
 @app.route('/')
-def upload_files():    
+def upload_files():
     return render_template('form.html')
 
-@app.route('/uploader', methods = ['POST'])
-def uploader():    
-    if request.method == "POST":    
-        
-        id_client = request.form.get('id') 
+
+@app.route('/uploader', methods=['POST'])
+def uploader():
+    if request.method == "POST":
+
+        id_client = request.form.get('id')
         savephoto()
         data_hist = checkHistogram()
 
-        if data_hist['success'] == True:  
-            try:          
-                isFront, isBack  = CedulaDetection.detect(id_client)                           
-                if isFront != False or isBack != False: 
-                    
+        if data_hist['success'] == True:
+            try:
+                isFront, isBack = CedulaDetection.detect(id_client)
+                if isFront != False or isBack != False:
+
                     Data = OCR_cedula.scan(id_client)
                     if Data["success"] == False:
-                        Data = {"success": False, "mensaje": "Error en el reconocimiento de caracteres, la imagen está muy borrosa o de difícil lectura"}
-                        logging.warning("Imagen borrosa o de dificil lectura") 
+                        Data = {
+                            "success": False, "mensaje": "Error en el reconocimiento de caracteres, la imagen está muy borrosa o de difícil lectura"}
+                        logging.warning("Imagen borrosa o de dificil lectura")
                     else:
                         aux_data = Data
-                        logging.warning(f"id:{id_client}, {aux_data}")                   
-                        image_64_front, image_64_back, image_64_code = images_64_encode(id_client)                
+                        logging.warning(f"id:{id_client}, {aux_data}")
+                        image_64_front, image_64_back, image_64_code = images_64_encode(
+                            id_client)
                         if image_64_back != None:
-                            Data["Imagen Cedula Posterior"] = str(image_64_back)
+                            Data["Imagen Cedula Posterior"] = str(
+                                image_64_back)
                         if image_64_front != None:
                             Data["Imagen Cedula Frontal"] = str(image_64_front)
                         if image_64_code != None:
                             Data["Imagen Codigo"] = str(image_64_code)
-                        
 
                     if isFront == True and isBack == True:
                         try:
-                            os.remove(save_path + f'/cedula_frontal_{id_client}.jpg')
-                            os.remove(save_path + f'/cedula_posterior_{id_client}.jpg')
-                            os.remove(save_path + f'/codigo_{id_client}.jpg')  
+                            os.remove(
+                                save_path + f'/cedula_frontal_{id_client}.jpg')
+                            os.remove(
+                                save_path + f'/cedula_posterior_{id_client}.jpg')
+                            os.remove(save_path + f'/codigo_{id_client}.jpg')
                         except:
                             print("No existe imagen")
 
                     if isFront == False and isBack == True:
-                        try:    
-                            os.remove(save_path + f'/codigo_{id_client}.jpg')                    
-                            os.remove(save_path + f'/cedula_posterior_{id_client}.jpg')                        
+                        try:
+                            os.remove(save_path + f'/codigo_{id_client}.jpg')
+                            os.remove(
+                                save_path + f'/cedula_posterior_{id_client}.jpg')
                         except:
                             print("No existe imagen")
-                    
 
-                if isFront == False and isBack == False:  
-                    Data = {"success": False, "mensaje": "No se detectó una cédula en la imagen"}
-                    logging.warning("No se detectó cédula")  
+                if isFront == False and isBack == False:
+                    Data = {"success": False,
+                            "mensaje": "No se detectó una cédula en la imagen"}
+                    logging.warning("No se detectó cédula")
 
                 return jsonify(Data)
             except:
-                Data = {"success": False, "mensaje": "No se puede procesar la cédula correctamente, tome la foto de nuevo"}
-                logging.warning("No se puede procesar la cédula correctamente")  
+                Data = {
+                    "success": False, "mensaje": "No se puede procesar la cédula correctamente, tome la foto de nuevo"}
+                logging.warning("No se puede procesar la cédula correctamente")
                 return jsonify(Data)
-            
-        else:            
+
+        else:
             return jsonify(data_hist)
 
-@app.route('/checkselfie', methods = ['POST'])
-def checkphoto():         
-    savephoto()
-    data = checkHistogram()  
 
-    if data["success"] == True: 
+@app.route('/checkselfie', methods=['POST'])
+def checkphoto():
+    savephoto()
+    data = checkHistogram()
+
+    if data["success"] == True:
         selfie = cv2.imread(save_path + "/Front.jpg")
-        h, w , c = selfie.shape
+        h, w, c = selfie.shape
         aux_selfie = selfie[200:h-170, 40:w-40]
-        aux_selfie = cv2.detailEnhance(aux_selfie, sigma_s=2, sigma_r=0.10) 
+        aux_selfie = cv2.detailEnhance(aux_selfie, sigma_s=2, sigma_r=0.10)
         cv2.imwrite(save_path + '/selfie.jpg', aux_selfie)
 
-        with open(save_path + '/selfie.jpg', "rb") as image_selfie:            
+        with open(save_path + '/selfie.jpg', "rb") as image_selfie:
             image_64_selfie = base64.b64encode(image_selfie.read())
             data["selfie"] = str(image_64_selfie)
 
-        try:                        
+        try:
             os.remove(save_path + '/selfie.jpg')
         except:
             print("No existe imagen")
-    
+
     return jsonify(data)
 
 
 def checkHistogram():
-    data = {}  
+    data = {}
 
-    img_check = cv2.imread(save_path + "/Front.jpg", 0)    
-    hist = cv2.calcHist([img_check], [0], None, [256], [0, 256])  
+    img_check = cv2.imread(save_path + "/Front.jpg", 0)
+    hist = cv2.calcHist([img_check], [0], None, [256], [0, 256])
 
     underExposer = hist[0:25]
     overExposer = hist[225:256]
 
     count_underExpose = 0
     for i in range(len(underExposer)):
-        if underExposer[i] >= 12000:
+        if underExposer[i] >= 20000:
             count_underExpose += 1
 
-    if count_underExpose >= 10: 
-        logging.warning("La imagen cuenta con poca iluminación, sitúese en un sitio más iluminado para tomar la foto")  
+    if count_underExpose >= 10:
+        logging.warning(
+            "La imagen cuenta con poca iluminación, sitúese en un sitio más iluminado para tomar la foto")
         data["mensaje"] = "La imagen cuenta con poca iluminación, sitúese en un sitio más iluminado para tomar la foto"
         data["success"] = False
 
     count_overExpose = 0
     for i in range(len(overExposer)):
-        if overExposer[i] >= 10000:
+        if overExposer[i] >= 20000:
             count_overExpose += 1
 
-    if count_overExpose >= 15:  
-        logging.warning("La imagen cuenta con mucha iluminación, sitúese en un sitio menos iluminado para tomar la foto") 
+    if count_overExpose >= 15:
+        logging.warning(
+            "La imagen cuenta con mucha iluminación, sitúese en un sitio menos iluminado para tomar la foto")
         data["mensaje"] = "La imagen cuenta con mucha iluminación, sitúese en un sitio menos iluminado para tomar la foto"
-        data["success"] = False 
+        data["success"] = False
 
     if count_overExpose < 15 and count_underExpose < 10:
         logging.warning("Exito, buena foto")
@@ -136,22 +147,23 @@ def checkHistogram():
 
     return data
 
+
 def savephoto():
-    count = 0    
+    count = 0
 
     if not os.path.exists(save_path):
-        os.makedirs(save_path) 
-    
-    files = request.files.getlist("files[]")                      
-    for file in files:                   
-        if count == 0:                
-            file.filename = "Front.jpg"  
+        os.makedirs(save_path)
+
+    files = request.files.getlist("files[]")
+    for file in files:
+        if count == 0:
+            file.filename = "Front.jpg"
         if file.filename == '':
-            break           
-        if count >= 1:                
+            break
+        if count >= 1:
             file.filename = "Back.jpg"
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))            
-        count += 1     
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        count += 1
 
 
 def images_64_encode(id_client):
@@ -159,7 +171,7 @@ def images_64_encode(id_client):
     image_64_back = None
     image_64_code = None
 
-    files = os.listdir(save_path) 
+    files = os.listdir(save_path)
 
     if f"cedula_frontal_{id_client}.jpg" in files:
         with open(save_path + f'/cedula_frontal_{id_client}.jpg', "rb") as image_front:
@@ -172,10 +184,9 @@ def images_64_encode(id_client):
     if f"codigo_{id_client}.jpg" in files:
         with io.open(save_path + f'/codigo_{id_client}.jpg', "rb") as image_code:
             image_64_code = base64.b64encode(image_code.read())
-     
+
     return image_64_front, image_64_back, image_64_code
 
-if __name__ == '__main__':    
-    app.run(debug = False, host="0.0.0.0", port=4000)
 
-
+if __name__ == '__main__':
+    app.run(debug=False, host="0.0.0.0", port=4000)
